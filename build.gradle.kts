a0a26d632cb5930f64b55bf424f2598ac57e2a0a
Gopal S Akshintala <gopalakshintala@gmail.com>
2022-07-07 10:56:54 +0530

    gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.diffplug.spotless.extra.wtp.EclipseWtpFormatterStep.XML
+
 plugins {
   java
-  id("com.adarshr.test-logger") version "3.1.0"
+  id("com.adarshr.test-logger") version "3.2.0"
+  id("com.diffplug.spotless") version "6.4.2"
 }
 
 group = "ga.overfullstack"
@@ -10,17 +13,51 @@ repositories {
   mavenCentral()
 }
 
-val immutablesVersion = "2.9.0"
+spotless {
+  kotlin {
+    target("src/main/java/**/*.kt", "src/test/java/**/*.kt")
+    targetExclude("$buildDir/generated/**/*.*")
+    ktlint().userData(mapOf("indent_size" to "2", "continuation_indent_size" to "2"))
+  }
+  kotlinGradle {
+    target("*.gradle.kts")
+    ktlint().userData(mapOf("indent_size" to "2", "continuation_indent_size" to "2"))
+  }
+  java {
+    toggleOffOn()
+    target("src/main/java/**/*.java", "src/test/java/**/*.java")
+    targetExclude("$buildDir/generated/**/*.*")
+    importOrder()
+    removeUnusedImports()
+    googleJavaFormat()
+    trimTrailingWhitespace()
+    indentWithSpaces(2)
+    endWithNewline()
+  }
+  format("xml") {
+    targetExclude("pom.xml")
+    target("*.xml")
+    eclipseWtp(XML)
+  }
+  format("documentation") {
+    target("*.md", "*.adoc")
+    trimTrailingWhitespace()
+    indentWithSpaces(2)
+    endWithNewline()
+  }
+}
+
 dependencies {
+  val immutablesVersion = "2.9.0"
   annotationProcessor("org.immutables:value:$immutablesVersion")
   compileOnly("org.immutables:builder:$immutablesVersion")
   compileOnly("org.immutables:value-annotations:$immutablesVersion")
-  implementation("io.vavr:vavr:+")
-  implementation("org.slf4j:slf4j-api:+")
+  implementation("io.vavr:vavr:0.10.4")
+  implementation("org.slf4j:slf4j-api:1.7.36")
   implementation("org.springframework:spring-beans:+")
   implementation("org.springframework:spring-context:+")
 
-  runtimeOnly("org.apache.logging.log4j:log4j-slf4j18-impl:+")
+  runtimeOnly("org.apache.logging.log4j:log4j-slf4j18-impl:2.17.1")
   testImplementation("org.mockito:mockito-core:+")
   testImplementation(platform("org.junit:junit-bom:+"))
   testImplementation("org.junit.jupiter:junit-jupiter-api")
@@ -29,25 +66,12 @@ dependencies {
 
 java.sourceCompatibility = JavaVersion.VERSION_17
 
-tasks.test {
-  useJUnitPlatform()
-  ignoreFailures = true
-}
-
-testlogger {
-  setTheme("mocha")
-  showExceptions = true
-  showStackTraces = false
-  showFullStackTraces = false
-  showCauses = false
-  slowThreshold = 2000
-  showSummary = true
-  showSimpleNames = true
-  showPassed = true
-  showSkipped = true
-  showFailed = true
-  showStandardStreams = false
-  showPassedStandardStreams = false
-  showSkippedStandardStreams = false
-  showFailedStandardStreams = false
+tasks {
+  test {
+    useJUnitPlatform()
+    ignoreFailures = true
+  }
+  testlogger {
+    theme = com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA_PARALLEL
+  }
 }
